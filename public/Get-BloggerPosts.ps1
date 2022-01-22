c36d3025353f1893bbb0c3ad4452e897c8e08d46
Bryan B. Cook <3217452+bryanbcook@users.noreply.github.com>
2022-01-22 15:26:50 -0500

    initial implementation for get blog posts

--- public/Get-BloggerPosts.ps1
+++ public/Get-BloggerPosts.ps1
@@ -2,8 +2,28 @@ Function Get-BloggerPosts
 {
     [CmdletBinding()]
     param(
-        [string]$blogid
+        [string]$BlogId,
+
+        [ValidateSet("draft","live","scheduled")]
+        [string]$Status = "live"
     )
 
-    $uri = "https://"
+    if (!$PSBoundParameters.ContainsKey("BlogId"))
+    {
+      $BlogId = $TyporaBloggerSession.BlogId
+      if (0 -eq $BlogId) {
+        throw "BlogId not specified."
+      }
+    }
+
+    try {
+        $uri = "https://www.googleapis.com/blogger/v3/blogs/$BlogId/posts?status=$status"
+    
+        $result = Invoke-GApi -uri $uri
+    
+        $result.items            
+    }
+    catch {
+        Write-Error $_.ToString()
+    }
 }
